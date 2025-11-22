533ab7e0773f6a7cf0588c72a3827bb1cce1a156
Phillip Bailey <ztron500@gmail.com>
2025-11-22 18:46:34 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
+const geistMono = Montserrat({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistMono.variable} antialiased`}
       >
         {children}
       </body>
