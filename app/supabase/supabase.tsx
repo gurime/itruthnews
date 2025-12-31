a360d014d488bb055e7fbdfa25657f005da103b7
Phillip Bailey <ztron500@gmail.com>
2025-12-31 13:19:48 -0500

    update

--- app/supabase/supabase.tsx
+++ app/supabase/supabase.tsx
@@ -1,17 +1,8 @@
-// supabase/supabase.ts
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-      storage: typeof window !== 'undefined' ? window.localStorage : undefined,
-    },
-  }
-)
-
-export default supabase
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
